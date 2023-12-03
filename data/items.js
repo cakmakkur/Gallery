d8f871c820ebbf77813a3903af0a31df25f487b7
Your Name <you@example.com>
2023-12-03 01:35:58 +0100

    first

---
+++ data/items.js
@@ -0,0 +1,207 @@
+export const items = [
+  {
+    id: '1',
+    image: 'painting-1.jpeg',
+    info: 'Dissolved <br />Oil and acryl on canvas <br />60x80'
+  },
+  {
+    id: '2',
+    image: 'painting-2.jpeg',
+    info: 'Faint <br />Oil on canvas <br />50x70'
+  },
+  {
+    id: '3',
+    image: 'painting-3.jpeg',
+    info: 'Encrypted <br />oil on canvas <br />50x70'
+  },
+  {
+    id: '4',
+    image: 'painting-4.jpeg',
+    info: 'Void <br />Oil on canvas <br />18x24'
+  },
+  {
+    id: '5',
+    image: 'painting-5.jpeg',
+    info: 'Reflexion <br />Ink on paper <br />21x29'
+  },
+  {
+    id: '6',
+    image: 'painting-6.jpeg',
+    info: 'Bloom <br />Oil on canvas <br />50x70'
+  },
+  {
+    id: '7',
+    image: 'painting-7.jpeg',
+    info: 'The earth slides <br />Oil on canvas <br />40x50'
+  },
+  {
+    id: '8',
+    image: 'painting-8.jpeg',
+    info: 'Auf der Spur <br />Oil on canvas <br />70x50'
+  },
+  {
+    id: '9',
+    image: 'painting-9.jpeg',
+    info: 'Besuch <br />Oil on canvas <br />70x50'
+  },
+  {
+    id: '10',
+    image: 'painting-10.jpeg',
+    info: 'Distant <br />Acryl on canvas <br />50x70'
+  },
+  {
+    id: '11',
+    image: 'painting-11.jpeg',
+    info: 'Somewhere sometime <br />Oil on canvas <br />60x40'
+  },
+  {
+    id: '12',
+    image: 'painting-12.jpeg',
+    info: 'Surrounded <br />Oil on canvas <br />40x50'
+  },
+  {
+    id: '13',
+    image: 'painting-13.jpeg',
+    info: 'Erkläre! <br />Oil on canvas <br />40x80'
+  },
+  {
+    id: '14',
+    image: 'painting-14.jpeg',
+    info: 'Ohrwurm <br />Oil on canvas <br />50x70'
+  },
+  {
+    id: '15',
+    image: 'painting-15.jpeg',
+    info: 'Blues <br />Oil on canvas <br />50x70'
+  },
+  {
+    id: '16',
+    image: 'painting-16.jpeg',
+    info: 'Turning back <br />Oil on canvas <br />60x40'
+  },
+  {
+    id: '17',
+    image: 'painting-17.jpeg',
+    info: 'Away now <br />Oil on canvas <br />60x40'
+  },
+  {
+    id: '18',
+    image: 'painting-18.jpeg',
+    info: 'Travellers <br />Digital'
+  },
+  {
+    id: '19',
+    image: 'painting-19.jpeg',
+    info: 'There will be it <br />Ink on paper <br />21x29'
+  },
+  {
+    id: '20',
+    image: 'painting-20.jpeg',
+    info: 'Raum <br />Oil on canvas <br />50x70'
+  },
+  {
+    id: '21',
+    image: 'painting-21.jpeg',
+    info: 'Vom Strand 2 <br />Oil on canvas <br />30x40'
+  },
+  {
+    id: '22',
+    image: 'painting-22.jpeg',
+    info: 'Genesis <br />Clay <br />15x15'
+  },
+  {
+    id: '23',
+    image: 'painting-23.jpeg',
+    info: 'Vom Strand 3 <br />Oil on canvas <br />30x40'
+  },
+  {
+    id: '24',
+    image: 'painting-24.jpeg',
+    info: 'Vom Strand 1 <br />Oil on canvas <br />30x40'
+  },
+  {
+    id: '25',
+    image: 'painting-25.jpeg',
+    info: 'Vom Strand 4 <br />Oil on canvas <br />30x40'
+  },
+  {
+    id: '26',
+    image: 'painting-26.jpeg',
+    info: 'Peace <br />Oil on canvas <br />80x30'
+  },
+  {
+    id: '27',
+    image: 'painting-27.jpeg',
+    info: 'Dissolved 2 <br />Acryl on canvas <br />40x20'
+  },
+  {
+    id: '28',
+    image: 'painting-28.jpeg',
+    info: 'Der Gedanke und seine Implikationen <br />Pastel on paper <br />21x29'
+  },
+  {
+    id: '29',
+    image: 'painting-29.jpeg',
+    info: 'Das Gespräch und seine Implikationen <br />Pastel on Paper <br />25x35'
+  },
+  {
+    id: '30',
+    image: 'painting-30.jpeg',
+    info: 'Einzug <br />Acryl on wood <br />60x60'
+  },
+  {
+    id: '31',
+    image: 'painting-31.jpeg',
+    info: 'Will you tell me? <br />Ink on paper <br />25x35'
+  },
+  {
+    id: '32',
+    image: 'painting-32.jpeg',
+    info: 'Wechsel <br />Earth and glass'
+  },
+  {
+    id: '33',
+    image: 'painting-33.jpeg',
+    info: 'Battle breeze <br />Digital'
+  },
+  {
+    id: '34',
+    image: 'painting-34.jpeg',
+    info: 'Away now 2 <br />Acryl on canvas <br />40x80'
+  },
+  {
+    id: '35',
+    image: 'painting-35.jpeg',
+    info: 'Not that old <br />Clay'
+  },
+  {
+    id: '36',
+    image: 'painting-36.jpeg',
+    info: 'Stadt auf Säure <br />Oil on canvas <br />50x70'
+  },
+  {
+    id: '37',
+    image: 'painting-37.jpeg',
+    info: 'Zeitklar <br />Oil on cardboard <br />40x30'
+  },
+  {
+    id: '38',
+    image: 'painting-38.jpeg',
+    info: 'Just met <br />Oil on canvas <br />40x40'
+  },
+  {
+    id: '39',
+    image: 'painting-39.jpeg',
+    info: 'Der Schatten <br />Ink and watercolor on paper <br />30x20'
+  },
+  {
+    id: '40',
+    image: 'painting-40.jpeg',
+    info: 'Der Gedanke 2 <br />Digital'
+  },
+  {
+    id: '41',
+    image: 'painting-41.jpeg',
+    info: 'Ausflug <br />Oil on canvas <br />50x40'
+  },
+]
---
+++ scripts/script.js
@@ -0,0 +1,125 @@
+import { items } from "../data/items.js";
+
+let isOpen = false;
+
+const sideBar = document.querySelector('.sidebar');
+const main = document.querySelector('.main');
+
+const menuButton = document.querySelector('.menuButton');
+const burgerItem = document.querySelector('.burgerItem');
+
+const menuItems = document.querySelectorAll('.menuItem');
+
+menuButton.addEventListener('mouseover', hoverColor);
+menuButton.addEventListener('mouseout', hoverColor);
+menuButton.addEventListener('click', toggleSidemenu);
+
+function toggleSidemenu () {
+  if (!isOpen) {
+    sideBar.classList.add('open');
+    main.classList.add('open');
+    burgerItem.classList.add('open');
+    menuItems.forEach((item) => {
+      item.classList.add('open');
+    })
+    isOpen = true;
+  } else {
+    sideBar.classList.remove('open');
+    main.classList.remove('open');
+    burgerItem.classList.remove('open');
+    menuItems.forEach((item) => {
+      item.classList.remove('open');
+    })
+    isOpen =false;
+  }
+};
+
+function hoverColor () {
+  burgerItem.classList.toggle('outsideHover');
+};
+
+let productsHTML = '';
+
+items.forEach((item) => {
+  productsHTML += `
+    <div class="productContainer product${item.id}">
+      <div class="productImage">
+        <img class="itemImage" data-image-id="${item.id}" src="paintings /${item.image}" alt="" />
+      </div>
+      <div class="productInfo">
+        ${item.info}
+        <svg
+          class="q-mark"
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 -960 960 960"
+          width="24"
+        >
+          <path
+            fill="currentColor"
+            d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"
+          />
+        </svg>
+      </div>
+    </div>
+    <div class="modal" data-id="${item.id}">
+        <img src="paintings /${item.image}" class="modal-content">
+    </div>
+  `
+})
+
+document.querySelector('.productGridContainer').innerHTML = productsHTML;
+
+
+//Header Scroll Behaviour
+
+const headerBar = document.querySelector('.header');
+let prevScrollPos = window.scrollY;
+
+window.onscroll = function () {
+  let currentScrollPos = window.scrollY;
+  if (prevScrollPos > currentScrollPos) {
+    headerBar.style.top = "0"
+  } else if (prevScrollPos > 40 && prevScrollPos < currentScrollPos) {
+    headerBar.style.top = "-20vh"
+  }
+  prevScrollPos = currentScrollPos;
+}
+
+//Full Screen Image
+
+const itemImages = document.querySelectorAll('.itemImage');
+const modals = document.querySelectorAll('.modal');
+
+itemImages.forEach((image) => {
+  const imageId = image.dataset.imageId;
+
+  modals.forEach((modal) => {
+    const modalId = modal.dataset.id;
+    if (modalId === imageId) {
+      image.addEventListener('click', () => {
+        modal.style.display = "flex";
+        headerBar.style.display = 'none';
+      })
+    }
+  })
+})
+
+window.onclick = function(event) {
+  modals.forEach((modal) => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+      headerBar.style.display = 'flex';
+    }
+  }) 
+};
+
+window.onkeydown = function(event) {
+  modals.forEach((modal) => {
+    if (event.key === 'Escape' || event.key === 'Esc' || event.keyCode === 27) {
+      modal.style.display = "none";
+      headerBar.style.display = 'flex';
+    }
+  }) 
+}
+
