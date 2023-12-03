80dc0a7cc18d1891a0285bd83f768af329c53153
Your Name <you@example.com>
2023-12-04 00:58:42 +0100

    comingUp

--- comingUp/comingUp.js
+++ comingUp/comingUp.js
@@ -1,14 +1,28 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   var textElement = document.querySelector('.textAnm');
-//   var textContent = textElement.textContent;
+const langButton = document.querySelector('.languageButton');
+const title = document.querySelector('.title');
+const info = document.querySelector('.info');
+const link = document.querySelector('.link');
 
-//   textElement.textContent = '';
+let currentLanguage = 'de'
 
-//   for (let i = 0; i < textContent.length; i++) {
-//     setTimeout(function(index) {
-//       return function() {
-//         textElement.textContent += textContent[index];
-//       };
-//     }(i), i * 40);
-//   }
-// });
+function changeLanguage () {
+  if (currentLanguage === 'en') {
+    title.innerHTML = `<h1>Kommt bald</h1>`;
+    info.innerHTML = `Diese Seite befindet sich derzeit im Aufbau <br />
+    Echt keine Ahnung wann sie fertig sein wird aber wenn es soweit ist wird es legendär! <br />Also komm später wieder`
+    link.innerHTML = `Zurück zur <span class="gal">Gallerie</span>`;
+    langButton.innerHTML = 'EN';
+    currentLanguage = 'de';
+  } else {
+    title.innerHTML = `<h1>Coming Up</h1>`;
+    info.innerHTML = `This page is currently under construction. <br />
+    We don't know yet when it will be ready but once it is, it will be
+    pretty awesome! <br />Please come back later...`
+    link.innerHTML = `Turn back to the <span class="gal">Gallery</span>`;
+    langButton.innerHTML = 'DE';
+    currentLanguage = 'en';
+  }
+}
+
+function renderPage () {changeLanguage();};
+renderPage();
