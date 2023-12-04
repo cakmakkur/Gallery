13754438d1e46a38c4b2287b94eb233e1a74c705
Your Name <you@example.com>
2023-12-04 11:17:18 +0100

    4.12-1

--- data/items.js
+++ data/items.js
@@ -2,206 +2,206 @@ export const items = [
   {
     id: '1',
     image: 'painting-1.jpeg',
-    info: 'Dissolved <br />Oil and acryl on canvas <br />60x80'
+    info: '<span class="nameTag">Dissolved</span>Oil and acryl on canvas<span class="sizeTag">60x80</span>'
   },
   {
     id: '2',
     image: 'painting-2.jpeg',
-    info: 'Faint <br />Oil on canvas <br />50x70'
+    info: '<span class="nameTag">Faint</span>Oil on canvas <br /><span class="sizeTag">50x70</span>'
   },
   {
     id: '3',
     image: 'painting-3.jpeg',
-    info: 'Encrypted <br />oil on canvas <br />50x70'
+    info: '<span class="nameTag">Encrypted</span>oil on canvas <br /><span class="sizeTag">50x70</span>'
   },
   {
     id: '4',
     image: 'painting-4.jpeg',
-    info: 'Void <br />Oil on canvas <br />18x24'
+    info: '<span class="nameTag">Void</span>Oil on canvas <br /><span class="sizeTag">18x24</span>'
   },
   {
     id: '5',
     image: 'painting-5.jpeg',
-    info: 'Reflexion <br />Ink on paper <br />21x29'
+    info: '<span class="nameTag">Reflexion </span>Ink on paper <br /><span class="sizeTag">21x29</span>'
   },
   {
     id: '6',
     image: 'painting-6.jpeg',
-    info: 'Bloom <br />Oil on canvas <br />50x70'
+    info: '<span class="nameTag">Bloom </span>Oil on canvas <br /><span class="sizeTag">50x70</span>'
   },
   {
     id: '7',
     image: 'painting-7.jpeg',
-    info: 'The earth slides <br />Oil on canvas <br />40x50'
+    info: '<span class="nameTag">The earth slides </span>Oil on canvas <br /><span class="sizeTag">40x50</span>'
   },
   {
     id: '8',
     image: 'painting-8.jpeg',
-    info: 'Auf der Spur <br />Oil on canvas <br />70x50'
+    info: '<span class="nameTag">Auf der Spur </span>Oil on canvas <br /><span class="sizeTag">70x50</span>'
   },
   {
     id: '9',
     image: 'painting-9.jpeg',
-    info: 'Besuch <br />Oil on canvas <br />70x50'
+    info: '<span class="nameTag">Besuch </span>Oil on canvas <br /><span class="sizeTag">70x50</span>'
   },
   {
     id: '10',
     image: 'painting-10.jpeg',
-    info: 'Distant <br />Acryl on canvas <br />50x70'
+    info: '<span class="nameTag">Distant </span>Acryl on canvas <br /><span class="sizeTag">50x70</span>'
   },
   {
     id: '11',
     image: 'painting-11.jpeg',
-    info: 'Somewhere sometime <br />Oil on canvas <br />60x40'
+    info: '<span class="nameTag">Somewhere sometime </span>Oil on canvas <br /><span class="sizeTag">60x40</span>'
   },
   {
     id: '12',
     image: 'painting-12.jpeg',
-    info: 'Surrounded <br />Oil on canvas <br />40x50'
+    info: '<span class="nameTag">Surrounded </span>Oil on canvas <br /><span class="sizeTag">40x50</span>'
   },
   {
     id: '13',
     image: 'painting-13.jpeg',
-    info: 'Erkläre! <br />Oil on canvas <br />40x80'
+    info: '<span class="nameTag">Erkläre! </span>Oil on canvas <br /><span class="sizeTag">40x80</span>'
   },
   {
     id: '14',
     image: 'painting-14.jpeg',
-    info: 'Ohrwurm <br />Oil on canvas <br />50x70'
+    info: '<span class="nameTag">Ohrwurm </span>Oil on canvas <br /><span class="sizeTag"><span class="sizeTag">50x70</span>'
   },
   {
     id: '15',
     image: 'painting-15.jpeg',
-    info: 'Blues <br />Oil on canvas <br />50x70'
+    info: '<span class="nameTag">Blues </span>Oil on canvas <br /><span class="sizeTag">50x70</span>'
   },
   {
     id: '16',
     image: 'painting-16.jpeg',
-    info: 'Turning back <br />Oil on canvas <br />60x40'
+    info: '<span class="nameTag">Turning back </span>Oil on canvas <br /><span class="sizeTag">60x40</span>'
   },
   {
     id: '17',
     image: 'painting-17.jpeg',
-    info: 'Away now <br />Oil on canvas <br />60x40'
+    info: '<span class="nameTag">Away now </span>Oil on canvas <br /><span class="sizeTag">60x40</span>'
   },
   {
     id: '18',
     image: 'painting-18.jpeg',
-    info: 'Travellers <br />Digital'
+    info: '<span class="nameTag">Travellers </span>Digital'
   },
   {
     id: '19',
     image: 'painting-19.jpeg',
-    info: 'There will be it <br />Ink on paper <br />21x29'
+    info: '<span class="nameTag">There will be it </span>Ink on paper <br /><span class="sizeTag">21x29</span>'
   },
   {
     id: '20',
     image: 'painting-20.jpeg',
-    info: 'Raum <br />Oil on canvas <br />50x70'
+    info: '<span class="nameTag">Raum </span>Oil on canvas <br /><span class="sizeTag">50x70</span>'
   },
   {
     id: '21',
     image: 'painting-21.jpeg',
-    info: 'Vom Strand 2 <br />Oil on canvas <br />30x40'
+    info: '<span class="nameTag">Vom Strand 2 </span>Oil on canvas <br /><span class="sizeTag">30x40</span>'
   },
   {
     id: '22',
     image: 'painting-22.jpeg',
-    info: 'Genesis <br />Clay <br />15x15'
+    info: '<span class="nameTag">Genesis </span>Clay <br /><span class="sizeTag">15x15</span>'
   },
   {
     id: '23',
     image: 'painting-23.jpeg',
-    info: 'Vom Strand 3 <br />Oil on canvas <br />30x40'
+    info: '<span class="nameTag">Vom Strand 3 </span>Oil on canvas <br /><span class="sizeTag">30x40</span>'
   },
   {
     id: '24',
     image: 'painting-24.jpeg',
-    info: 'Vom Strand 1 <br />Oil on canvas <br />30x40'
+    info: '<span class="nameTag">Vom Strand 1 </span>Oil on canvas <br /><span class="sizeTag">30x40</span>'
   },
   {
     id: '25',
     image: 'painting-25.jpeg',
-    info: 'Vom Strand 4 <br />Oil on canvas <br />30x40'
+    info: '<span class="nameTag">Vom Strand 4 </span>Oil on canvas <br /><span class="sizeTag">30x40</span>'
   },
   {
     id: '26',
     image: 'painting-26.jpeg',
-    info: 'Peace <br />Oil on canvas <br />80x30'
+    info: '<span class="nameTag">Peace </span>Oil on canvas <br /><span class="sizeTag">80x30</span>'
   },
   {
     id: '27',
     image: 'painting-27.jpeg',
-    info: 'Dissolved 2 <br />Acryl on canvas <br />40x20'
+    info: '<span class="nameTag">Dissolved 2 </span>Acryl on canvas <br /><span class="sizeTag">40x20</span>'
   },
   {
     id: '28',
     image: 'painting-28.jpeg',
-    info: 'Der Gedanke und seine Implikationen <br />Pastel on paper <br />21x29'
+    info: '<span class="nameTag">Der Gedanke und seine Implikationen </span>Pastel on paper <br /><span class="sizeTag">21x29</span>'
   },
   {
     id: '29',
     image: 'painting-29.jpeg',
-    info: 'Das Gespräch und seine Implikationen <br />Pastel on Paper <br />25x35'
+    info: '<span class="nameTag">Das Gespräch und seine Implikationen </span>Pastel on Paper <br /><span class="sizeTag">25x35</span>'
   },
   {
     id: '30',
     image: 'painting-30.jpeg',
-    info: 'Einzug <br />Acryl on wood <br />60x60'
+    info: '<span class="nameTag">Einzug </span>Acryl on wood <br /><span class="sizeTag">60x60</span>'
   },
   {
     id: '31',
     image: 'painting-31.jpeg',
-    info: 'Will you tell me? <br />Ink on paper <br />25x35'
+    info: '<span class="nameTag">Will you tell me? </span>Ink on paper <br /><span class="sizeTag">25x35</span>'
   },
   {
     id: '32',
     image: 'painting-32.jpeg',
-    info: 'Wechsel <br />Earth and glass'
+    info: '<span class="nameTag">Wechsel </span>Earth and glass'
   },
   {
     id: '33',
     image: 'painting-33.jpeg',
-    info: 'Battle breeze <br />Digital'
+    info: '<span class="nameTag">Battle breeze </span>Digital'
   },
   {
     id: '34',
     image: 'painting-34.jpeg',
-    info: 'Away now 2 <br />Acryl on canvas <br />40x80'
+    info: '<span class="nameTag">Away now 2 </span>Acryl on canvas <br /><span class="sizeTag">40x80</span>'
   },
   {
     id: '35',
     image: 'painting-35.jpeg',
-    info: 'Not that old <br />Clay'
+    info: '<span class="nameTag">Not that old </span>Clay'
   },
   {
     id: '36',
     image: 'painting-36.jpeg',
-    info: 'Stadt auf Säure <br />Oil on canvas <br />50x70'
+    info: '<span class="nameTag">Stadt auf Säure </span>Oil on canvas <br /><span class="sizeTag">50x70</span>'
   },
   {
     id: '37',
     image: 'painting-37.jpeg',
-    info: 'Zeitklar <br />Oil on cardboard <br />40x30'
+    info: '<span class="nameTag">Zeitklar </span>Oil on cardboard <br /><span class="sizeTag">40x30</span>'
   },
   {
     id: '38',
     image: 'painting-38.jpeg',
-    info: 'Just met <br />Oil on canvas <br />40x40'
+    info: '<span class="nameTag">Just met </span>Oil on canvas <br /><span class="sizeTag">40x40</span>'
   },
   {
     id: '39',
     image: 'painting-39.jpeg',
-    info: 'Der Schatten <br />Ink and watercolor on paper <br />30x20'
+    info: '<span class="nameTag">Der Schatten </span>Ink and watercolor on paper <br /><span class="sizeTag">30x20</span>'
   },
   {
     id: '40',
     image: 'painting-40.jpeg',
-    info: 'Der Gedanke 2 <br />Digital'
+    info: '<span class="nameTag">Der Gedanke 2 </span>Digital'
   },
   {
     id: '41',
     image: 'painting-41.jpeg',
-    info: 'Ausflug <br />Oil on canvas <br />50x40'
+    info: '<span class="nameTag">Ausflug </span>Oil on canvas <br /><span class="sizeTag">50x40</span>'
   },
 ]
